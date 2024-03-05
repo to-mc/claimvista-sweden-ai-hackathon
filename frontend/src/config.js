caa96af50f2416460e674976c2f17018f956c8af
Emil Nildersen <emil@gr8c.se>
2024-03-05 16:22:34 +0100

    removed demo URL and added placeholders

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,11 +1,9 @@
 const config = {
-  API_BASE_URL:
-    "https://eu-central-1.aws.data.mongodb-api.com/app/aimngoclaims-ieeyr/endpoint",
-  CHART_BASE_URL:
-    "https://charts.mongodb.com/charts-tom-mccarthy-demo-project-giexs",
-  CHART_ID: "65e5f2bd-904d-46f8-8edb-57967c61d00f",
-  CHART_ID1: "65e5ee1d-7178-4151-8ca7-5a92e75f1316",
-  CHART_ID2: "65e5f61e-7a78-48cd-87ad-2452652fb58a",
+  API_BASE_URL: "<<Add your base URL from app-services>>/endpoint",
+  CHART_BASE_URL: "<<Add base URL from charts>>",
+  CHART_ID: "",
+  CHART_ID1: "",
+  CHART_ID2: "",
 };
 
 export default config;
