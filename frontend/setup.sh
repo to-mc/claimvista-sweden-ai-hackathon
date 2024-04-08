08f20fc45a62bba2389d77eaae3d4ec4ef37ef7c
Pierre Petersson <pierre@thinkworks.se>
2024-04-08 11:17:37 +0200

    Added creation of secrets and updated documentation.

--- frontend/setup.sh
+++ frontend/setup.sh
@@ -6,4 +6,6 @@ appservices apps create -y -n "$APPLICATION_NAME" --project="$ATLAS_PROJECT_ID" 
 description=$(appservices apps describe --app $APPLICATION_NAME)
 APP_ID=$(echo $description | sed 's/^.*client_app_id": "\([^"]*\).*/\1/')
 echo "updating APP_ID to $APP_ID"
+appservices secrets create --app="$APP_ID" --project="$ATLAS_PROJECT_ID" --name="azure-openai-key" --value="$AZURE_OPENAI_API_KEY"
+appservices secrets create --app="$APP_ID" --project="$ATLAS_PROJECT_ID" --name="azure-endpoint" --value="$AZURE_OPENAI_ENDPOINT" 
 sed -i -E 's/\(REALM_CLIENT_APP_ID=\).*/\1'$APP_ID'/g' env.var
